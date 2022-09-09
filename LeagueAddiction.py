c4b938f4e3deef0371c3e8b31cb66a0c5a74def3
henryye20 <97543867+henryye20@users.noreply.github.com>
2022-09-09 10:28:56 -0700

    Update LeagueAddiction.py

--- LeagueAddiction.py
+++ LeagueAddiction.py
@@ -4,7 +4,7 @@ import cassiopeia as cass
 
 
 
-cass.set_riot_api_key("RGAPI-7a90ab5a-fdeb-47d5-ab09-c69fee9e3bc8")  # This overrides the value set in your configuration/settings.
+cass.set_riot_api_key("")  # This overrides the value set in your configuration/settings.
 
 summoner = cass.get_summoner(name="pandacreeper", region="NA")
 print("{name} is a level {level} summoner on the {region} server.".format(name=summoner.name,
