da4412027c244a49d5dad0d8759dc73f408f661e
Henry <henryye2006@gmail.com>
2022-09-15 10:04:43 -0700

    made dateCheck and timeAdd functions and changed to arrow module for time

--- LeagueAddiction.py
+++ LeagueAddiction.py
@@ -1,26 +1,33 @@
 import random
-import datetime
+import arrow
 import config
 import cassiopeia as cass
-
-
-def dateCheck():
-    print('hi')
+limit = 1
 cass.set_riot_api_key(config.api_key)  # This overrides the value set in your configuration/settings.
+summoner = cass.get_summoner(name="nicthequick", region="NA")
 
-cass.set_riot_api_key("")  # This overrides the value set in your configuration/settings.
+def dateCheck(x,y):
+    if(x.year==y.year and x.month==y.month and x.day==y.day): return True
+    else: return False
+def timeAdd():
+    for z in range(20):
+        print(summoner.match_history[z].duration)
 
-summoner = cass.get_summoner(name="pandacreeper", region="NA")
+
 print("{name} is a level {level} summoner on the {region} server.".format(name=summoner.name,
                                                                           level=summoner.level,
                                                                           region=summoner.region))
-person = summoner(name="pandacreeper", region="NA")
-match = person.match_history[0]
-champion_played = match.participants[person].champion
+
+match = summoner.match_history[0]
+champion_played = match.participants[summoner].champion
 lengthOfMatch = match.duration
-dateOfMatch = match.start
+dateOfMatch = match.start.to('US/Pacific')
 print(champion_played.name)
-print(dateOfMatch.format('YYYY-MM-DD'))
-dateToday = datetime.date.today()
-print(match.type())
-print(dateToday.type())
+print(dateOfMatch)
+#.format('YYYY-MM-DD')
+utc = arrow.utcnow()
+now = utc.to('US/Pacific')
+print(match.duration)
+print(now)
+print(dateCheck(dateOfMatch,now))
+timeAdd()
