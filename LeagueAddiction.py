2c2cffc06a72b1c37b1066b733eb556d44083d12
Henry <henryye2006@gmail.com>
2022-09-20 10:29:12 -0700

    coded a gui beginning input works but the second winow breaks bcause of loops

--- LeagueAddiction.py
+++ LeagueAddiction.py
@@ -4,7 +4,11 @@ import config
 import cassiopeia as cass
 from time import sleep
 import kill
-limit = -1
+import PySimpleGUIQt as sg
+import LeagueAddiction as la
+global stop 
+stop = False
+Mlimit = -1
 cass.set_riot_api_key(config.api_key)  # This overrides the value set in your configuration/settings.
 summoner = cass.get_summoner(name="nicthequick", region="NA")
 
@@ -17,43 +21,101 @@ def dateCheck(x,y):
     else: return False
 
 #adds all the time of matches played today
-def timeAddT():
+def timeAddT(nam,reg):
+    summoner = cass.get_summoner(name=nam, region=reg)
     total = 0
-    f = 0.0
-    for z in range(11):
+    f = 0
+    for z in range(10):
         match = summoner.match_history[z]
         dateOfMatch = match.start.to('US/Pacific')
-
+        
         if not (dateCheck(now,dateOfMatch)):
             continue
-
+    
         f += match.duration.seconds
     return f
 
-Flag = False
-Flag2 = False
-First = True
-while(True):
-    t = timeAddT()
-    p = 0
-    if(Flag):
-        sleep(3600)
-    
-    while(t>limit):
-        kill.leaguekill()
-        sleep(2)
-        p += 2
-        if(p==3600):
-            Flag2 = True
+def timed(name,region,lim):
+    #checks if totaltime played today is greater than the limit every 10 min
+    Tlimit = lim
+    while not stop:
+        print("loop")
+        flag = True
+        t = timeAddT(name,region)
+        if(t==-10): 
+            print('ended early')
+            break 
+        p = 0
+        while(t>Tlimit):
+            kill.leaguekill()
+            sleep(1)
+            p += 1
+            if(stop):break
+            if(p==120):
+                flag = False
+                break
+        if(stop):break
+        if flag: sleep(2)   
+
+sg.theme('GreenMono')
+def second(x,y,z):
+    layout = [
+    [
+        sg.Text("Click to Start"),
+    ],
+    [
+        sg.Button("Start",key = "start"),
+    ],
+    [
+        sg.Button("Stop",key = "stop"),
+    ],
+        ]
+    window = sg.Window("Second Window", layout)
+    stop = False
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
             break
-    if(Flag2):
-        Flag = False
-        Flag2 = False
-        continue
-    if(First):
-        sleep(3600)
-        First = False
-    Flag = True
+        #doesnt work 
+        if event == "start":
+            stop = False
+        if event == "stop":
+            stop = True
+            break
+        
+    window.close()
+
+
+def first():
+    layout = [
+    [
+        sg.Text("Input Summoner Name:"),
+        sg.In(size=(25, 1)) 
+    ],
+    [
+        sg.Text("Input Region:"),
+        sg.In(size=(6, 1))
+    ],
+    [
+        sg.Text("Input Limit (seconds):"),
+        sg.In(size=(15, 1))
+    ],
+    [
+        sg.Button("Submit",key = "sub"),
+    ],
+        ]
+    window = sg.Window("League Addiction", layout)
+    while True:
+        event, values = window.read()
+        if event == "cancel" or event == sg.WIN_CLOSED:
+            break
+        if event =="sub":
+            second(values[0],values[1],int(values[2]))
+               
+    window.close()
+first()
+#timed("nicthequick","NA",40000)
+
 
 #print("{name} is a level {level} summoner on the {region} server.".format(name=summoner.name,
 #                                                                          level=summoner.level,
